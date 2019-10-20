f3c14f2348e4133fa12074ceb259a18ac55ad4b2
Stas <lukyanov.stas@mail.ru>
2019-10-20 18:20:37 +0200

    First bot's code

---
+++ bot.php
@@ -0,0 +1,9 @@
+<?php
+
+include "vk_api.php";
+
+const VK_KEY = "ff9ac4821606597362378976df339ff99a63f20a0d366d99b7aed060ef8eabaf4aecab7082846eb6779fa";
+const ACCESS_KEY= "7f108fe4"; // Ключ из сообщества
+const VERSION = "5.102";  // Версия API VK 
+
+?>
